5b0ec4c489cee854d619e5faf907c4aa8550c5a7
Hazem Sayed <hazem.world123@gmail.com>
2018-08-04 15:03:47 -0500

    Add files via upload

---
+++ twitter_app.py
@@ -0,0 +1,42 @@
+import socket
+import sys
+import requests
+import requests_oauthlib
+import json
+
+ACCESS_TOKEN = '153918738-SQi9h0I8UWA4ABmZOyiZyWP8ilD39kfAXQfGqSFT'
+ACCESS_SECRET = 'T5nuktfWRiRICE5RmvAalGuDOILJ3AB9pMPQn98vrzkZC'
+CONSUMER_KEY = 'f2P1qTi4YioChDeALucTREBfX'
+CONSUMER_SECRET = 'Lidx9DszAoVpzRo6QGyJwUhzceZ5hOxe7SkMI9xw5EulKMzmaS'
+my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_TOKEN, ACCESS_SECRET)
+def get_tweets():
+	url = 'https://stream.twitter.com/1.1/statuses/filter.json'
+	query_data = [('track','wordlcup,worldcup2018,russia2018,soccer,league')]
+	query_url = url + '?' + '&'.join([str(t[0]) + '=' + str(t[1]) for t in query_data])
+	response = requests.get(query_url, auth=my_auth, stream=True)
+	print(query_url, response)
+	return response
+
+def send_tweets_to_spark(http_resp, tcp_connection):
+	for line in http_resp.iter_lines():
+	    try:
+             	full_tweet = json.loads(line.decode("utf-8"))
+             	tweet_text = full_tweet["text"]
+             	print("Tweet Text: " + tweet_text)
+             	print ("------------------------------------------")
+             	tcp_connection.send(tweet_text.encode("utf-8") + '\n'.encode("utf-8"))
+	    except:
+        	e = sys.exc_info()[0]
+        	print("Error: %s" % e)
+
+TCP_IP = "localhost"
+TCP_PORT = 9009
+conn = None
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+print("Waiting for TCP connection...")
+conn, addr = s.accept()
+print("Connected... Starting getting tweets.")
+resp = get_tweets()
+send_tweets_to_spark(resp, conn)
